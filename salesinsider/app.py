4753d89e00cb47879f6e0e7f05761aed149a3974
MuratCKOC <muratcagrikoc@gmail.com>
2021-03-17 16:38:07 -0400

    removed notebooks

--- salesinsider/app.py
+++ salesinsider/app.py
@@ -43,29 +43,29 @@ def index():
 #         for rows in csvReader:
 
 
-@app.route('/<pagename>')
-def admin(pagename):
-    return render_template(pagename+'.html')
+# @app.route('/<pagename>')
+# def admin(pagename):
+#     return render_template(pagename+'.html')
 
 
-@app.route('/<path:resource>')
-def serveStaticResource(resource):
-	return send_from_directory('static/', resource)
+# @app.route('/<path:resource>')
+# def serveStaticResource(resource):
+# 	return send_from_directory('static/', resource)
 
 
-@app.route('/test')
-def test():
-    return '<strong>It\'s Alive!</strong>'
+# @app.route('/test')
+# def test():
+#     return '<strong>It\'s Alive!</strong>'
 
 
-@app.errorhandler(jinja2.exceptions.TemplateNotFound)
-def template_not_found(e):
-    return not_found(e)
+# @app.errorhandler(jinja2.exceptions.TemplateNotFound)
+# def template_not_found(e):
+#     return not_found(e)
 
 
-@app.errorhandler(404)
-def not_found(e):
-    return '<strong>Page Not Found!</strong>', 404
+# @app.errorhandler(404)
+# def not_found(e):
+#     return '<strong>Page Not Found!</strong>', 404
     
 if __name__ == "__main__":
     app.run()
