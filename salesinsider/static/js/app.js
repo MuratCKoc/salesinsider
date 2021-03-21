d442b10ef786a5092e2aa649ed71d8c5fc945771
MuratCKOC <muratcagrikoc@gmail.com>
2021-03-20 23:51:40 -0400

    wordCLoud sample

--- salesinsider/static/js/app.js
+++ salesinsider/static/js/app.js
@@ -1,3 +1,5 @@
+
+
 // d3 bindings to html
 var options = d3.select("#select")
 var selector = d3.select("#selDataset");
@@ -20,6 +22,7 @@ function init() {
     //     buildMetadata(initialSample)
     // });
     get_Calculations()
+    create_WordCloud()
 };
 
 function get_Calculations(){
@@ -27,7 +30,9 @@ function get_Calculations(){
     // console.log(results)
     d3.json(url).then((data) => {
         var columns = Object.keys(data)
+        // Remove Dates
         columns.shift()
+        get_count(data)
         var PATTERN = /_Predicted/
         predicted_columns = columns.filter(function (str) { return PATTERN.test(str); });
         predicted_columns.forEach((item) => {
@@ -37,6 +42,10 @@ function get_Calculations(){
         })
     })
     console.log(url)
+}
+
+function get_count() {
+    console.log(data)
 }
 
 function get_mean(arr) {
@@ -78,26 +87,11 @@ init();
 
 function build_Charts(initialSample) {
 
-
     imgUrl = '/static/images/plots/'+initialSample+".png"
     console.log("IMAGE URL", imgUrl)
 
     plotarea.append("p").text(imgUrl)
-    plotarea.append("img").attr({'src='})
-// var svg = plotarea.append('svg').attr({
-//   width: 300,
-//   height: 300,
-//   border: '1px solid #ccc'
-// });
-
-// svg.append('svg:image')
-// .attr({
-//   'xlink:href': 'http://www.iconpng.com/png/beautiful_flat_color/computer.png',  // can also add svg file here
-//   x: 0,
-//   y: 0,
-//   width: 128,
-//   height: 128
-// });
+    
     console.log("ASDASD", initialSample)
 }
 //     d3.json("data/prophet1.json").then((data) => {
@@ -192,3 +186,51 @@ function build_Charts(initialSample) {
 
 // // Event listener
 options.on("change", init());
+
+function create_WordCloud() {
+    // List of words
+var myWords = [{word: "Running", size: "10"}, {word: "Surfing", size: "20"}, {word: "Climbing", size: "50"}, {word: "Kiting", size: "30"}, {word: "Sailing", size: "20"}, {word: "Snowboarding", size: "60"} ]
+
+// set the dimensions and margins of the graph
+var margin = {top: 10, right: 10, bottom: 10, left: 10},
+    width = 450 - margin.left - margin.right,
+    height = 450 - margin.top - margin.bottom;
+
+// append the svg object to the body of the page
+var svg = d3.select("#wordCloud").append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+  .append("g")
+    .attr("transform",
+          "translate(" + margin.left + "," + margin.top + ")");
+
+// Constructs a new cloud layout instance. It run an algorithm to find the position of words that suits your requirements
+// Wordcloud features that are different from one word to the other must be here
+var layout = d3.layout.cloud()
+  .size([width, height])
+  .words(myWords.map(function(d) { return {text: d.word, size:d.size}; }))
+  .padding(5)        //space between words
+  .rotate(function() { return ~~(Math.random() * 2) * 90; })
+  .fontSize(function(d) { return d.size; })      // font size of words
+  .on("end", draw);
+layout.start();
+
+// This function takes the output of 'layout' above and draw the words
+// Wordcloud features that are THE SAME from one word to the other can be here
+function draw(words) {
+  svg
+    .append("g")
+      .attr("transform", "translate(" + layout.size()[0] / 2 + "," + layout.size()[1] / 2 + ")")
+      .selectAll("text")
+        .data(words)
+      .enter().append("text")
+        .style("font-size", function(d) { return d.size; })
+        .style("fill", "#69b3a2")
+        .attr("text-anchor", "middle")
+        .style("font-family", "Impact")
+        .attr("transform", function(d) {
+          return "translate(" + [d.x, d.y] + ")rotate(" + d.rotate + ")";
+        })
+        .text(function(d) { return d.text; });
+}
+}
