39ddebb462a57d9ed0cdcfc3b03251a78ae74361
MuratCKOC <muratcagrikoc@gmail.com>
2021-03-25 14:22:45 -0400

    retry

--- salesinsider/app.py
+++ salesinsider/app.py
@@ -18,8 +18,9 @@ from sqlalchemy.orm import Session
 from sqlalchemy import create_engine
 
 # Import config
-from config import database_url
+#from config import database_url
 
+database_url = 'postgres://jxjswcgzrdbbwg:478c9797d5ce92f765d973a886f93de59af3de94ed90feacd3ab8c8af8601276@ec2-3-223-72-172.compute-1.amazonaws.com:5432/d85kivepj9hj9h'
 
 #==================================
 # Create an engine for the database
